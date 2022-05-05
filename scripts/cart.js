f9adfb5d8c821dca3a8bff52785a083299937325
S-Thor <sahil.thoriya96@gmail.com>
2022-05-05 17:12:09 +0530

    update

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,2 +1,32 @@
 
+if (arr.length == 0)
+{
+    empty();
+} else
+{
+    showProduct();
+}
+
+
+function empty() {
+    let msg = document.createElement("h2");
+    msg.setAttribute("id","emptyMsg");
+    msg.innerText = "Your shopping cart is empty."
+
+    document.getElementsByClassName(".counter").innerText = "(0)";
+    document.getElementsByClassName(".total").innerText = "$0.00";
+    document.getElementById("finalTotal").innerText = "$0.00";
+}
+
+function showProduct() {
+    document.getElementById("productImage").src = url;
+    document.getElementById("productName").innerText = "name";
+    document.getElementById("productPrice").innerText = price;
+
+    document.getElementsByClassName(".counter").innerText = "(${length})";
+    document.getElementsByClassName(".total").innerText = total;
+    document.getElementById("finalTotal").innerText = total;
+}
+
+
 
