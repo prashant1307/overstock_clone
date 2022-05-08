baa73f48966ce34f3757bd01d84e8e6d6e1d76b5
prashant1307 <prashantlorgev8@gmail.com>
2022-05-08 11:26:22 +0530

    added navbar

--- product.js
+++ product.js
@@ -1,3 +1,10 @@
+import {topmost,navbar,bottom_nav} from '../components/navbar.js';
+document.getElementById('navbar').innerHTML=navbar()
+document.getElementById('topmost').innerHTML=topmost()
+document.getElementById('bottom_nav').innerHTML=bottom_nav()
+
+
+
 let productData = [
 
     {
