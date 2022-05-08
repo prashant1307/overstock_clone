5a2b48f0569c10164737aaebabe3fee46a165ece
priyodas1997 <priyo.honey.1997@gmail.com>
2022-05-08 13:11:09 +0530

    Update payment.js

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,15 +1,15 @@
-var cartProducts = [{image: "https://ak1.ostkcdn.com/images/products/is/images/direct/46a2619bf7e2515d8d79a4b5e658a5a3f78d02d1/LUCID-Comfort-Collection-10-inch-Luxury-Gel-Memory-Foam-Mattress.jpg?impolicy=mediumlow",
-                     name: "LUCID Comfort Collection 10-inch Luxury Gel Memory Foam Mattress",
-                     price: 99.99,
-                     category: "Chair"}];
-
+// var cartProducts = [{image: "https://ak1.ostkcdn.com/images/products/is/images/direct/46a2619bf7e2515d8d79a4b5e658a5a3f78d02d1/LUCID-Comfort-Collection-10-inch-Luxury-Gel-Memory-Foam-Mattress.jpg?impolicy=mediumlow",
+//                      name: "LUCID Comfort Collection 10-inch Luxury Gel Memory Foam Mattress",
+//                      price: 99.99,
+//                      category: "Chair"}];
+var cartProducts = JSON.parse(localStorage.getItem("cart")) || [];
 var total = cartProducts.reduce(function(sum,elem){
     return sum + Number(elem.price);
 }, 0);
 
 document.querySelector(".counter").innerText = `(${cartProducts.length})`;
 document.getElementById("total").innerText = `$${total}`;
-document.getElementById("finalTotal").innerText = `$${total}`;
+document.getElementById("finalTotal").innerText = `₹${total}`;
 
 
 document.querySelector("form").addEventListener("submit", getDetails);
